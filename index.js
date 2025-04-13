bbd457cee9d2e996ab9bd05394d750eabcbf77dd
Tayyabaankalgi <tayyabaankalgi@gmail.com>
2025-04-13 22:27:16 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+var numberofDrumButtons = document.querySelectorAll(".drum").length;
+
+for(var i=0; i<numberofDrumButtons; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+       var audio= new audio("sounds/tom-1.mp3");
+       audio.play();
+    });
+}
